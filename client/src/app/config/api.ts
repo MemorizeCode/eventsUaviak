c2a2cf75c46b5130fb7d855614b2657f43cb9080
MemorizeCode <ilya.bezrukov2005@mail.ru>
2025-03-23 17:00:53 +0400

    upd docker nginx api

--- client/src/app/config/api.ts
+++ client/src/app/config/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
 const $api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${import.meta.env.VITE_API_BACKEND}`,
   headers:{
       "Content-Type": "application/json",
   },
